d2e6b3687918bf28ade92618682980fe98399ba0
jimmyma <jimmyma@croxlink.com>
2019-10-07 16:56:32 +0800

    init answer

---
+++ README.md
@@ -0,0 +1,25 @@
+2.請問依序列印 list 1-3 結果是如何？為何？ ( Python 3 ) :
+```
+def add_val_to_list (val, list=[]) :  
+	list.append(val)
+	return list
+
+list1 = add_val_to_list( 321 )
+list2 = add_val_to_list( 123 ,[])
+list3 = add_val_to_list( 'a' )
+```
+
+3.請說明造成以下兩者輸出的差異原因？若輸入值維持不變讓Python輸出的結果和PHP一致你會如何修改（Python 3）？
+
+PHP
+```
+$input = ['banana' => 1, 'apple' => 2, 'orange' => 3];
+echo json_encode($input);
+```
+Python
+
+```
+import json
+input = {'banana': 1, 'apple': 2, 'orange': 3}
+print(json.dumps(input))
+```
---
+++ answer3.py
@@ -0,0 +1,34 @@
+'''
+3.請說明造成以下兩者輸出的差異原因？
+若輸入值維持不變讓Python輸出的結果和PHP一致你會如何修改（Python 3）？
+'''
+import json
+
+input = {'banana': 1, 'apple': 2, 'orange': 3}
+
+'''
+PHP code:
+$input = ['banana' => 1, 'apple' => 2, 'orange' => 3];
+echo json_encode($input);
+
+輸出結果為:
+{"banana":1,"apple":2,"orange":3}
+'''
+print('PHP code的輸出:')
+print('{"banana":1,"apple":2,"orange":3}')
+
+'''
+題目 Python code的輸出結果為:
+{"banana": 1, "apple": 2, "orange": 3}
+'''
+print('原本Python code的輸出:')
+print(json.dumps(input))
+
+'''
+答案 Python code的輸出結果為:
+{"banana":1,"apple":2,"orange":3}
+因為json.dumps在處理dic時會在value前多一個" "空格
+所以就把": "用":"取代即可
+'''
+print('我的Python做法:')
+print(json.dumps(input).replace(": ", ":"))
