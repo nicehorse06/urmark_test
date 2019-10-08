931ac5b38a202414f3845721c7e12ceff8976e31
Jimmy Ma <a468963@gmail.com>
2019-10-08 23:30:36 +0800

    update answer2

--- answer2.py
+++ answer2.py
@@ -5,12 +5,12 @@ def add_val_to_list(val, this_list=[]):
 
 print('----print list1----')
 list1 = add_val_to_list(321)
-print(list1)
+print(list1) # [321]
 
 print('----print list2----')
 list2 = add_val_to_list(123, [])
-print(list2)
+print(list2) # [123]
 
 print('----print list3----')
 list3 = add_val_to_list('a')
-print(list3)
+print(list3) # [321, 'a']
